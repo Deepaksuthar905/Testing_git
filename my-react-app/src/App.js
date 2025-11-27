077f85ded2b8a278bd8bc499dc13e35e4f414086
Deepak Suthar <deepak2003suthar@gmail.com>
2025-11-27 18:19:42 +0530

    normal changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,13 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px", fontSize: "30px" }}>
       <h1>Hello Deepak 👋</h1>
+      <h1>Hello dilip 👋</h1>
+      <h1>Hello bhanu 👋</h1>
+      <h1>Hello varu 👋</h1>
+
+      <h1>Hello mahipal 👋</h1>
+      <h1>Hello sir 👋</h1>
+
       <p>Welcome to your first React project!</p>
     </div>
   );
